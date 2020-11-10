b3bebeaa421e0f223a9cab0564f41df5acb6bdbf
LanHao <bigpangl@163.com>
2020-11-10 16:01:31 +0800

    最终目的是通过拉伸+布尔运算得到点云数据.现在在布尔运算阶段一方面慢，另一方面结果不准确.暂时无法使用

--- stairs.py
+++ stairs.py
@@ -71,34 +71,34 @@ for mesh in meshs:
     else:
         csg = csg.to_union(csg_mid)
 #
-# add_length = 0
-# open_holes = [{"x": 300.0, "y": 100.0, "z": 257, "r": 30.0, "d": {"x": 0, "y": 0, "z": -1},
-#                "length": 257 + add_length},
-#               {"x": 970.0, "y": 100.0, "z": 257.88366668686183, "r": 30, "d": {"x": 0, "y": 0, "z": -1},
-#                "length": 257 + add_length},
-#               {"x": 300.0, "y": 2680.0, "z": 1590.883666686862, "r": 30, "d": {"x": 0, "y": 0, "z": -1},
-#                "length": 168 + add_length},
-#               {"x": 970.0, "y": 2680.0, "z": 1590.883666686862, "r": 30, "d": {"x": 0, "y": 0, "z": -1},
-#                "length": 168 + add_length}]
-# # mesh = Open3dTranslate.to_mesh(csg.to_triangles())
+add_length = 0
+open_holes = [{"x": 300.0, "y": 100.0, "z": 257, "r": 30.0, "d": {"x": 0, "y": 0, "z": -1},
+               "length": 257 + add_length},
+              {"x": 970.0, "y": 100.0, "z": 257.88366668686183, "r": 30, "d": {"x": 0, "y": 0, "z": -1},
+               "length": 257 + add_length},
+              {"x": 300.0, "y": 2680.0, "z": 1590.883666686862, "r": 30, "d": {"x": 0, "y": 0, "z": -1},
+               "length": 168 + add_length},
+              {"x": 970.0, "y": 2680.0, "z": 1590.883666686862, "r": 30, "d": {"x": 0, "y": 0, "z": -1},
+               "length": 168 + add_length}]
+# mesh = Open3dTranslate.to_mesh(csg.to_triangles())
 # # mesh = mesh.sample_points_uniformly(number_of_points=300000)
 # #
 # # o3d.visualization.draw_geometries([mesh])
 #
 holes = []
 #
-# for hole in open_holes:
-#     hole_mesh = o3d.geometry.TriangleMesh.create_cylinder(hole["r"], hole["length"])
-#     hole_mesh.translate((hole["x"], hole["y"], hole["z"] - hole["length"] / 2))
-#     # holes.append(hole_mesh)
-#     #
-#     csg_mid = CSG.from_trianagles(Open3dTranslate.to_triangles(hole_mesh))
-#     logging.debug(f"open hole 开口初始化CSG 完成")
-#     if csg is None:
-#         csg = csg_mid
-#     else:
-#         csg = csg.to_subtract(csg_mid)
-#     logging.debug(f"完成一个开口")
+for hole in open_holes:
+    hole_mesh = o3d.geometry.TriangleMesh.create_cylinder(hole["r"], hole["length"])
+    hole_mesh.translate((hole["x"], hole["y"], hole["z"] - hole["length"] / 2))
+    # holes.append(hole_mesh)
+    #
+    csg_mid = CSG.from_trianagles(Open3dTranslate.to_triangles(hole_mesh))
+    logging.debug(f"open hole 开口初始化CSG 完成")
+    if csg is None:
+        csg = csg_mid
+    else:
+        csg = csg.to_subtract(csg_mid)
+    logging.debug(f"完成一个开口")
 mesh = Open3dTranslate.to_mesh(csg.to_triangles())
 mesh = mesh.sample_points_uniformly(number_of_points=900000)
 #
